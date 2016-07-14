f6643576d603b395c43d629d8b9f15f66d828a19
Marco Foganholo <marco@lojavirtual.digital>
2016-07-14 18:56:34 -0300

    Up Files
    Tradução, Correios e PagSeguro

---
+++ public/config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart55190.lojavirtual.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart55190.lojavirtual.local/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\Users\mfoganholo\Documents\GitHub\opencart55190\public/catalog/');
+define('DIR_SYSTEM', 'C:\Users\mfoganholo\Documents\GitHub\opencart55190\public/system/');
+define('DIR_LANGUAGE', 'C:\Users\mfoganholo\Documents\GitHub\opencart55190\public/catalog/language/');
+define('DIR_TEMPLATE', 'C:\Users\mfoganholo\Documents\GitHub\opencart55190\public/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\Users\mfoganholo\Documents\GitHub\opencart55190\public/system/config/');
+define('DIR_IMAGE', 'C:\Users\mfoganholo\Documents\GitHub\opencart55190\public/image/');
+define('DIR_CACHE', 'C:\Users\mfoganholo\Documents\GitHub\opencart55190\public/system/cache/');
+define('DIR_DOWNLOAD', 'C:\Users\mfoganholo\Documents\GitHub\opencart55190\public/system/download/');
+define('DIR_UPLOAD', 'C:\Users\mfoganholo\Documents\GitHub\opencart55190\public/system/upload/');
+define('DIR_MODIFICATION', 'C:\Users\mfoganholo\Documents\GitHub\opencart55190\public/system/modification/');
+define('DIR_LOGS', 'C:\Users\mfoganholo\Documents\GitHub\opencart55190\public/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'opencart55190');
+define('DB_PREFIX', 'oc_');
---
+++ public/system/modification/catalog/language/english/common/cart.php
@@ -0,0 +1,13 @@
+<?php
+// Text
+
+
+			$_['text_items2']    = '%s';
+			$_['text_shopping_cart']    = 'Meu Carrinho:';
+
+					$_['text_items']    = '%s items';
+
+$_['text_empty']    = 'Your shopping cart is empty!';
+$_['text_cart']     = 'View Cart';
+$_['text_checkout'] = 'Checkout';
+$_['text_recurring']  = 'Payment Profile';
